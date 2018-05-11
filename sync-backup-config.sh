a1e5d1a2c26327c477574238ab25f4be123df2a7
grewn0uille <lucieanglade.gm@gmail.com>
2018-05-11 15:54:02 +0200

    Add sync instruction

--- sync-backup-config.sh
+++ sync-backup-config.sh
@@ -11,3 +11,8 @@ typeset -A RCLONE_REPOS
 RCLONE_REPOS=(
     [0]="super-test ftp host toto.host.fr user totouser pass totopwd"
 )
+
+typeset -A RCLONE_SYNC
+RCLONE_SYNC=(
+    [0]="local_path remote:/path"
+)
