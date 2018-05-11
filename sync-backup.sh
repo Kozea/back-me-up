619b6c7e0a7a55b9b83ca8ff6134c69e704122f5
grewn0uille <lucieanglade.gm@gmail.com>
2018-05-11 15:54:38 +0200

    Sync with rclone

---
+++ sync-backup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#==============================================================================
+# FILE : sync-backup.sh
+# USAGE : 
+# DESCRIPTION : Sync local back-up with a remote server
+# NOTES : 
+#==============================================================================
+
+# Load shared variables and functions
+{
+    source "$(dirname "$0")/shared.sh" 
+} || {
+    echo -e "\\033[0;31mUnable to load shared.sh\\033[0m" && exit 1 
+}
+
+echo -e "${GREEN}sync-backup starts !${NC}"
+
+
+# Sync file with remote server
+if [[ ! -v RCLONE_REPOS[@] ]]
+then
+    echo -e "${YELLOW}No rclone repos defined${NC}"
+else
+    echo -e "* ${BLUE}Start synchronizing files${NC}"
+fi
+
+
+for rclone_conf in "${RCLONE_REPOS[@]}"
+do
+    remote_name=$(echo "$rclone_conf" | awk '{print $1}')
+    if [ "$(${RCLONE} listremotes | grep -c "$remote_name")" -lt 1 ]
+    then
+        echo -e "${YELLOW}The remote ${remote_name} doesn't exist${NC}"
+        echo -e "* ${BLUE}Initializing the remote${NC}"
+        {
+            ${RCLONE} config create "$rclone_conf"
+        } || {
+            echo -e "${RED}Failed to create the remote ${remote_name}${NC}" && exit 1
+        }
+    fi
+    for sync_instruction in "${RCLONE_SYNC[@]}"
+    do
+        {
+            echo -e "${BLUE}Sync ${sync_instruction}${NC}"
+            ${RCLONE} sync "${sync_instruction}"
+        } || {
+            echo -e "${YELLOW}Failed to sync ${sync_instruction}${NC}"
+        }
+    done
+done
+
+echo -e "${GREEN}Sync succeed !${NC}"
