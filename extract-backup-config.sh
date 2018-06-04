175470feed944c79a25e90f64bc74817f6c42147
grewn0uille <lucieanglade.gm@gmail.com>
2018-06-04 11:46:25 +0200

    Configfile example for extract-backup.sh

---
+++ extract-backup-config.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#==============================================================================
+# FILE : extract-backup-config.sh
+# USAGE : source extract-backup-config.sh
+# DESCRIPTION : Configfile example for extract-back-up.sh
+# NOTES : 
+#==============================================================================
+
+NAME1_PASSPHRASE="superpassphrase"
+NAME2_PASSPHRASE="superpassphrase"
+NAME3_PASSPHRASE="superpassphrase"
+
+NAME1_ARCHIVES_LOCATION="/path/where/backup/are/stored"
+NAME1_EXTRACT_PATH="/path/where/backup/are/extracted"
+
+typeset -A NAME1_ARCHIVES_TO_EXTRACT
+NAME1_ARCHIVES_TO_EXTRACT=(
+    [0]="archive-to-extract"
+    [1]="other-archive-to-extract"
+)
+typeset -A NAME1_POSTGRES_INSTRUCTIONS
+NAME1_POSTGRES_INSTRUCTIONS=(
+    [0]="${pg_restore} ..."
+    [1]="${pg_restore} ..."
+)
